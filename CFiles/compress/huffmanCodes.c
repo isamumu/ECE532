38eec0ede1e9b078b16157524bf52cb68ac76189
Isamu <isamu.poy@mail.utoronto.ca>
2021-02-02 22:00:55 +0900

    added grey

--- CFiles/compress/huffmanCodes.c
+++ CFiles/compress/huffmanCodes.c
@@ -60,73 +60,79 @@ int main()
 	else
 	{ 
 		
-		char filename[] = "lenagray.bmp";
-	int i,j,data,offset,hbytes,width,height;
-	long bmpsize=0,bmpdataoff=0,bpp=0;
-	int **image;
-	int temp=0;
-	FILE *image_file;
-	image_file = fopen(filename,"rb");
-	if(image_file==NULL)
-	{
-		printf("Error Opening File!!");
-		exit(1);
-	}
-	else
-	{
-		//Set file position of the stream to the beginning
-		printf("Processing BMP Header...\n\n");
-		offset = 0;
-		fseek(image_file,offset,SEEK_SET);
-		printf("Getting file ID...\n\n");
-		for(i=0;i<2;i++)
-		{
-			fread(&data,1,1,image_file);
-			printf("%c",data);
-		}
-		printf("\n\nGetting size of BMP File...\n\n");
-		fread(&bmpsize,4,1,image_file);
-		printf("Size of the BMP File:: %ld bytes\n\n",bmpsize);
-		printf("Getting offset where the pixel arrray starts...\n\n");
-		offset = 10;
-		fseek(image_file,offset,SEEK_SET);
-		fread(&bmpdataoff,4,1,image_file);
-		printf("Bitmap data offset:: %ld\n\n",bmpdataoff);
-		printf("DIB Header\n\n");
-		fread(&hbytes,4,1,image_file);
-		printf("Number of bytes in header:: %d\n\n",hbytes);
-		fread(&width,4,1,image_file);
-		fread(&height,4,1,image_file);
-		printf("Width of Image: %d\n",width);
-		printf("Height of image: %d\n\n",height);
-		fseek(image_file,2,SEEK_CUR);
-		fread(&bpp,2,1,image_file);
-		printf("Number of bits per pixel: %ld\n\n",bpp);
-		printf("Setting offset to start of pixel data...\n\n");
-		fseek(image_file,bmpdataoff,SEEK_SET);
-		printf("Creating Image array...\n\n");
-		int **image = (int **)malloc(height*sizeof(int *));
-		for(i=0;i<height;i++)
-		{
-			image[i] = (int *)malloc(width*sizeof(int));
-		}
-		int numbytes = (bmpsize - bmpdataoff)/3;
-		printf("Number of bytes: %d \n\n",numbytes);
-		int r,c;
-		printf("Reading the BMP File into Image Array...\n\n");
-		for(r=0;r<height;r++)
-		{
-			for(c=0;c<width;c++)
-			{
-				fread(&temp,3,1,image_file);
-				temp = temp&0x0000FF;
-				image[r][c] = temp;
-			}
-		}
-		printf("Reading the BMP File into Image Array Completed...\n\n");
+		// Set file position of the 
+		// stream to the beginning 
+		// Contains file signature 
+		// or ID "BM" 
+		offset = 0; 
+		
+		// Set offset to 2, which 
+		// contains size of BMP File 
+		offset = 2; 
+		
+		fseek(image_file, offset, SEEK_SET); 
+		
+		// Getting size of BMP File 
+		fread(&bmpsize, 4, 1, image_file); 
+		
+		// Getting offset where the 
+		// pixel array starts 
+		// Since the information is 
+		// at offset 10 from the start, 
+		// as given in BMP Header 
+		offset = 10; 
+		
+		fseek(image_file, offset, SEEK_SET); 
+		
+		// Bitmap data offset 
+		fread(&bmpdataoff, 4, 1, image_file); 
+		
+		// Getting height and width of the image 
+		// Width is stored at offset 18 and 
+		// height at offset 22, each of 4 bytes 
+		fseek(image_file, 18, SEEK_SET); 
+		
+		fread(&width, 4, 1, image_file); 
+		
+		fread(&height, 4, 1, image_file); 
+		
+		// Number of bits per pixel 
+		fseek(image_file, 2, SEEK_CUR); 
+		
+		fread(&bpp, 2, 1, image_file); 
+		
+		// Setting offset to start of pixel data 
+		fseek(image_file, bmpdataoff, SEEK_SET); 
+		
+		// Creating Image array 
+		image = (int**)malloc(height * sizeof(int*)); 
+		
+		for (i = 0; i < height; i++) 
+		{ 
+			image[i] = (int*)malloc(width * sizeof(int)); 
+		} 
+		
+		// int image[height][width] 
+		// can also be done 
+		// Number of bytes in 
+		// the Image pixel array 
+		int numbytes = (bmpsize - bmpdataoff) / 3; 
+		
+		// Reading the BMP File 
+		// into Image Array 
+		for (i = 0; i < height; i++) 
+		{ 
+			for (j = 0; j < width; j++) 
+			{ 
+				fread(&temp, 3, 1, image_file); 
+				
+				// the Image is a 
+				// 24-bit BMP Image 
+				temp = temp & 0x0000FF; 
+				image[i][j] = temp; 
+			} 
+		} 
 	} 
-
-	printf("")
 	
 	// Finding the probability 
 	// of occurrence 
