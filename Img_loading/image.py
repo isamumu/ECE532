0a307ac5e1898f59eafd027cf1f235fb879d8daa
Isamu <isamu.poy@mail.utoronto.ca>
2021-02-18 20:40:33 +0900

    added new folder

---
+++ Img_loading/image.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+import os.path
+
+def split(array, nrows, ncols):
+    """Split a matrix into sub-matrices."""
+
+    r, h = array.shape
+    return (array.reshape(h//nrows, nrows, -1, ncols)
+                 .swapaxes(1, 2)
+                 .reshape(-1, nrows, ncols))
+                 
+image = cv2.imread("cat.jpeg", 0)
+print(image.shape)
+arr = split(image,8,8)
+
+a_file = open("test.txt", "w")
+for row in image:
+    np.savetxt(a_file, row)
+
+a_file.close()
+
+
+
+
+
+# arr = np.asarray(image)
+# arr = np.split(arr, 10)
+# arr = np.array([np.split(x, 16, 1) for x in arr])
+# print(arr[1][1])
