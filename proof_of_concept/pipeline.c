6ecf7614bef8c6f99c1fe4a90b9cae6d271e15a4
isamumu <isamu510@hotmail.com>
2021-02-24 23:00:05 +0900

    pipeline mostly complete

--- proof_of_concept/pipeline.c
+++ proof_of_concept/pipeline.c
@@ -6,7 +6,7 @@
 
 int main (void)
 {
-    // int* input_image_row = (int*) malloc( PICTURE_SIZE * sizeof(int));
+    // int* input_image_row = (int*) malloc( BLOCK_SIZE * sizeof(int));
     // input_image_row[0] = 5;
     // input_image_row[1] = 10;
     // input_image_row[2] = 20;
@@ -26,6 +26,7 @@ int main (void)
         printf("Could not open file %s",filename);
         return 1;
     }
+
     int counter = 0;
     int x = 0;
     int y = 0;
@@ -51,7 +52,7 @@ int main (void)
     int c_size = 8;
     int num_chunks = (dim_y) / (c_size);
     double chunks[num_chunks][8][8];
-    int result_blks[num_chunks][8][8];
+    float result_blks[num_chunks][8][8];
 
     for(int j = 0; j < dim_y; j++){
         for(int k = 0; k < dim_x; k++){
@@ -62,67 +63,56 @@ int main (void)
     // ====================================================================
     
     for (int n = 0; n < num_chunks; n++){
-        int* input_image = (int*) malloc( PICTURE_SIZE * PICTURE_SIZE * sizeof(int));
+        int* input_image = (int*) malloc( BLOCK_SIZE * BLOCK_SIZE * sizeof(int));
         int i;
         int j;
-        // for (i = 0; i < PICTURE_SIZE * PICTURE_SIZE; i++)
-        // {
-        //     input_image[i] = i + 1;
-        // }
-        // for (i = 0; i < PICTURE_SIZE; i++)
-        // {
-        //     for (j = 0; j < PICTURE_SIZE; j++)
-        //     {    
-        //         printf ("Original Pixel %d %d: %d\n", i, j, input_image[8*i + j]);
-        //     }
-        // }
 
         // store the pixel values
         for (j = 0; j < c_size; j++) {
             for (int k = 0; k < c_size; k++) {
                 input_image[8*j+k] = (int) chunks[n][j][k];
-                printf("%d ", input_image[8*j+k]);
+                //printf("%d ", input_image[8*j+k]);
             }
         }
 
-        float* output_dct_coeffs = (float*) malloc( PICTURE_SIZE * PICTURE_SIZE * sizeof(float));
+        float* output_dct_coeffs = (float*) malloc( BLOCK_SIZE * BLOCK_SIZE * sizeof(float));
         compute_dct(input_image, output_dct_coeffs);
-        for (i = 0; i < PICTURE_SIZE; i++)
+        for (i = 0; i < BLOCK_SIZE; i++)
         {
-            for (j = 0; j < PICTURE_SIZE; j++)
+            for (j = 0; j < BLOCK_SIZE; j++)
             {    
                 printf ("DCT Coefficient %d %d: %.3f\n", i, j, output_dct_coeffs[8*i + j]);
             }
         }
 
         // Quantize DCT Coeff's
-        float* quantization_table = (float*) malloc( PICTURE_SIZE * PICTURE_SIZE * sizeof(float));
-        for (i = 0; i < PICTURE_SIZE; i++)
+        float* quantization_table = (float*) malloc( BLOCK_SIZE * BLOCK_SIZE * sizeof(float));
+        for (i = 0; i < BLOCK_SIZE; i++)
         {
-            for (j = 0; j < PICTURE_SIZE; j++)
+            for (j = 0; j < BLOCK_SIZE; j++)
             {    
                 quantization_table[8*i + j] = 2;
             }
         }
-        float* inverse_quantization_table = (float*) malloc( PICTURE_SIZE * PICTURE_SIZE * sizeof(float));
-        for (i = 0; i < PICTURE_SIZE; i++)
+        float* inverse_quantization_table = (float*) malloc( BLOCK_SIZE * BLOCK_SIZE * sizeof(float));
+        for (i = 0; i < BLOCK_SIZE; i++)
         {
-            for (j = 0; j < PICTURE_SIZE; j++)
+            for (j = 0; j < BLOCK_SIZE; j++)
             {    
                 inverse_quantization_table[8*i + j] = 0.5;
             }
         }
         quantizer(output_dct_coeffs, inverse_quantization_table);
-        for (i = 0; i < PICTURE_SIZE; i++)
+        for (i = 0; i < BLOCK_SIZE; i++)
         {
-            for (j = 0; j < PICTURE_SIZE; j++)
+            for (j = 0; j < BLOCK_SIZE; j++)
             {    
                 printf ("Quantized DCT Coefficient %d %d: %.3f\n", i, j, output_dct_coeffs[8*i + j]);
             }
         }
 
-        for(int i = 0; i < PICTURE_SIZE; i++){
-            for(j = 0; j < PICTURE_SIZE; j++){
+        for(int i = 0; i < BLOCK_SIZE; i++){
+            for(j = 0; j < BLOCK_SIZE; j++){
                 result_blks[n][i][j] = output_dct_coeffs[8*i + j];
             }
         }
@@ -137,105 +127,70 @@ int main (void)
     }
 
     int newblk [dim_x][dim_y];
-    
-    for(int j = 0; j < dim_y; j++){
-        for(int k = 0; k < dim_x; k++){
-            newblk[j][k] = result_blks[j/c_size][j][k];
-        }
-    }
-
-    for(int j = 0; j < dim_y; j++){
-        for(int k = 0; k < dim_x; k++){
-            printf("%d ", newblk[j][k]);
+       printf("==================== Quantized & DCT 8x8 blocks: ================================\n");
+    for(int n = 0; n < num_chunks; n++){
+        for(int j = 0; j < 8; j++){
+            for(int k = 0; k < 8; k++){
+                printf("%f ", result_blks[n][j][k]);
+            }
+            printf("\n");
         }
         printf("\n");
+        printf("------------------------------------------------------------\n");
+    }
+ 
+    for(int j = 0; j < dim_y; j++){
+        for(int k = 0; k < dim_x; k++){
+            //printf("%d ", newblk[j][k]);
+        }
+        //printf("\n");
     }
 
-    FILE *f = fopen("coeffs.txt", "w");
-     for(int j = 0; j < dim_y; j++){
-        for(int k = 0; k < dim_x; k++){
-            fprintf(fp, "%d\n", newblk[j][k]);
+    // Dequantization followed by inverse DCT
+    float results[num_chunks][8][8];
+    float* quantization_table = (float*) malloc( BLOCK_SIZE * BLOCK_SIZE * sizeof(float));
+    for (int i = 0; i < BLOCK_SIZE; i++)
+    {
+        for (int j = 0; j < BLOCK_SIZE; j++)
+        {    
+            quantization_table[8*i + j] = 2;
         }
     }
-    fclose(fp);
 
+    for(int n = 0; n < num_chunks; n++){
+        float* input_image = (float*) malloc( BLOCK_SIZE * BLOCK_SIZE * sizeof(int));
 
-    // // De-Quantize the coefficients
-    // dequantizer(output_dct_coeffs, quantization_table);
-    // for (i = 0; i < PICTURE_SIZE; i++)
-    // {
-    //     for (j = 0; j < PICTURE_SIZE; j++)
-    //     {    
-    //         printf ("Dequantized DCT Coefficient %d %d: %.3f\n", i, j, output_dct_coeffs[8*i + j]);
-    //     }
-    // }
+        for (int j = 0; j < c_size; j++) {
+            for (int k = 0; k < c_size; k++) {
+                input_image[8*j+k] = result_blks[n][j][k];
+            }
+        }
+        
+        dequantizer(input_image, quantization_table);
+        
+        float* inverted_pixels = (float*) malloc( BLOCK_SIZE * BLOCK_SIZE * sizeof(float));
+        
+        compute_inverse_dct(input_image, inverted_pixels);
 
-    // // Compute inverse DCT Coeffs
-    // float* inverted_pixels = (float*) malloc( PICTURE_SIZE * PICTURE_SIZE * sizeof(float));
-    // compute_inverse_dct(output_dct_coeffs, inverted_pixels);
-    // for (i = 0; i < PICTURE_SIZE; i++)
-    // {
-    //     for (j = 0; j < PICTURE_SIZE; j++)
-    //     {    
-    //         printf ("Inverted Pixel %d %d: %.3f\n", i, j, inverted_pixels[8*i + j]);
-    //     }
-    // }
+        for(int i = 0; i < BLOCK_SIZE; i++){
+            for(int j = 0; j < BLOCK_SIZE; j++){
+                results[n][i][j] = (int) inverted_pixels[8*i + j];
+            }
+        }
 
-    // test_zig_zag();
+        free(input_image);
+    }
 
-    // float* output_dct_coeffs = (float*) malloc( PICTURE_SIZE * sizeof(float));
-    // int l;
-    // for (l = 0; l < PICTURE_SIZE; l++)
-    // {
-    //     printf("L is %d\n", l);
-    //     output_dct_coeffs[l] = compute_dct_coeff(l, input_image_row);
-    //     printf("DCT Coefficient %d: %.3f\n", l, output_dct_coeffs[l]);
-    //     // compute_um(l, input_image_row, um_values);
-    //     // for (int i = 0; i < PICTURE_SIZE; i++)
-    //     // {
-    //     //     printf("%d\n", um_values[i]);
-    //     // }
-    // }
-
-
-
-    // // Compute inverse DCT Coeffs
-    // float* inverted_pixels = (float*) malloc( PICTURE_SIZE * sizeof(float));
-    // for (l = 0; l < PICTURE_SIZE / 2; l++)
-    // {
-    //     struct Tuple pixels = compute_inverse_dct_coeff(l, output_dct_coeffs);
-    //     inverted_pixels[l] = pixels.x_l;
-    //     inverted_pixels[PICTURE_SIZE-1-l] = pixels.x_l_other_end;
-    // }
-    // for (l = 0; l < PICTURE_SIZE; l++)
-    // {
-    //     printf ("Inverted Pixel %d: %.3f\n", l, inverted_pixels[l]);
-    // }
-    // // free (input_image_row);
-    // free (output_dct_coeffs); 
-    // free (inverted_pixels);
-
-    // float* transposed_matrix = (float*) malloc(64 * sizeof(float));
-    // int i;
-    // int j;
-    // for (i = 0; i < 64; i++)
-    // {
-    //     transposed_matrix[i] = i+1;
-    // }
-    // // for (i = 0; i < 8; i++)
-    // // {
-    // //     for (j = 0; j < 8; j++)
-    // //     {
-    // //         printf("%d %d: %.2f\n", i, j, transposed_matrix[8*i + j]);
-    // //     }
-    // // }
-    // transpose_matrix(transposed_matrix);
-    // for (i = 0; i < 8; i++)
-    // {
-    //     for (j = 0; j < 8; j++)
-    //     {
-    //         printf("%d %d: %.2f\n", i, j, transposed_matrix[8*i + j]);
-    //     }
-    // }
-    // free(transposed_matrix);
+    printf("==================== Dequantized & IDCT 8x8 blocks: ================================\n");
+    for(int n = 0; n < num_chunks; n++){
+        for(int j = 0; j < 8; j++){
+            for(int k = 0; k < 8; k++){
+                printf("%f ", results[n][j][k]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+        printf("------------------------------------------------------------\n");
+    }
+    
 }
