14e010b525f1cd890118e2789d11f304f4e15d9f
Isamu <isamu.poy@mail.utoronto.ca>
2021-03-07 23:58:12 +0900

    weird

--- Demo/pipeline.c
+++ Demo/pipeline.c
@@ -169,7 +169,7 @@ int main (void)
         for(int i = 0; i < BLOCK_SIZE; i++){
             for(int j = 0; j < BLOCK_SIZE; j++){
                 // result_blks[n][i][j] = encoded_bitstream[8*i + j];
-                result_blks[n + i * BLOCK_SIZE + j * BLOCK_SIZE] = encoded_bitstream[8*i + j];
+                //result_blks[n + i * BLOCK_SIZE + j * BLOCK_SIZE] = encoded_bitstream[8*i + j];
                 printf("%d ", n + i * BLOCK_SIZE + j * BLOCK_SIZE);
             }
         }
