a7426ec150228000d9085888058d0b4c6e0c270b
Isamu <isamu.poy@mail.utoronto.ca>
2021-03-10 22:24:00 +0900

    images

--- Demo/pipeline.c
+++ Demo/pipeline.c
@@ -47,7 +47,7 @@ int main (void)
     printf("here??");
     int c_size = 8;
     // int num_chunks = (dim_y) / (c_size);
-    int num_chunks = 30;
+    int num_chunks = 24;
     int chunks[num_chunks][8][8];
     float result_blks[num_chunks][8][8];
     printf("here?");
