d30c6935cc39acfa0c942b0763175dc7348c1aa3
Isamu <isamu.poy@mail.utoronto.ca>
2021-03-16 14:18:37 +0900

    mods

--- Demo/pipeline.c
+++ Demo/pipeline.c
@@ -53,7 +53,7 @@ int main (void)
     for(int j = 0; j < dim_y; j++){
         for(int k = 0; k < dim_x; k++){
             int var = floor(imagePix[j][k]);
-            chunks[n][j][k] = floor(1.234);
+            chunks[n][j][k] = var;
             if(j*k % 64 == 0){
                 n++;
             }
