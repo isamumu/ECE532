8433a59f5844110b823088b02d855c2ad9bf8cf5
isamumu <isamu510@hotmail.com>
2021-03-24 06:20:01 -0400

    Add files via upload

---
+++ integration/streamFIFO/Debug/src/helloworld.d
@@ -0,0 +1,58 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c \
+ ../../streamFIFO_bsp/microblaze_0/include/xparameters.h \
+ ../../streamFIFO_bsp/microblaze_0/include/xil_exception.h \
+ ../../streamFIFO_bsp/microblaze_0/include/xil_types.h \
+ ../../streamFIFO_bsp/microblaze_0/include/xstreamer.h \
+ ../../streamFIFO_bsp/microblaze_0/include/xenv.h \
+ ../../streamFIFO_bsp/microblaze_0/include/xenv_standalone.h \
+ ../../streamFIFO_bsp/microblaze_0/include/mb_interface.h \
+ ../../streamFIFO_bsp/microblaze_0/include/xil_assert.h \
+ ../../streamFIFO_bsp/microblaze_0/include/xil_exception.h \
+ ../../streamFIFO_bsp/microblaze_0/include/xparameters.h \
+ ../../streamFIFO_bsp/microblaze_0/include/xdebug.h \
+ ../../streamFIFO_bsp/microblaze_0/include/xil_cache.h \
+ ../../streamFIFO_bsp/microblaze_0/include/xllfifo.h \
+ ../../streamFIFO_bsp/microblaze_0/include/xstreamer.h \
+ ../../streamFIFO_bsp/microblaze_0/include/xllfifo_hw.h \
+ ../../streamFIFO_bsp/microblaze_0/include/xil_io.h \
+ ../../streamFIFO_bsp/microblaze_0/include/xil_printf.h \
+ ../../streamFIFO_bsp/microblaze_0/include/bspconfig.h \
+ ../../streamFIFO_bsp/microblaze_0/include/xstatus.h
+
+../../streamFIFO_bsp/microblaze_0/include/xparameters.h:
+
+../../streamFIFO_bsp/microblaze_0/include/xil_exception.h:
+
+../../streamFIFO_bsp/microblaze_0/include/xil_types.h:
+
+../../streamFIFO_bsp/microblaze_0/include/xstreamer.h:
+
+../../streamFIFO_bsp/microblaze_0/include/xenv.h:
+
+../../streamFIFO_bsp/microblaze_0/include/xenv_standalone.h:
+
+../../streamFIFO_bsp/microblaze_0/include/mb_interface.h:
+
+../../streamFIFO_bsp/microblaze_0/include/xil_assert.h:
+
+../../streamFIFO_bsp/microblaze_0/include/xil_exception.h:
+
+../../streamFIFO_bsp/microblaze_0/include/xparameters.h:
+
+../../streamFIFO_bsp/microblaze_0/include/xdebug.h:
+
+../../streamFIFO_bsp/microblaze_0/include/xil_cache.h:
+
+../../streamFIFO_bsp/microblaze_0/include/xllfifo.h:
+
+../../streamFIFO_bsp/microblaze_0/include/xstreamer.h:
+
+../../streamFIFO_bsp/microblaze_0/include/xllfifo_hw.h:
+
+../../streamFIFO_bsp/microblaze_0/include/xil_io.h:
+
+../../streamFIFO_bsp/microblaze_0/include/xil_printf.h:
+
+../../streamFIFO_bsp/microblaze_0/include/bspconfig.h:
+
+../../streamFIFO_bsp/microblaze_0/include/xstatus.h:
